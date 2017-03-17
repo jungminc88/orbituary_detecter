c5210ff3a41ce12ef74b62d875480bb4943f97be
jungminc88 <jungminc88@gmail.com>
2017-03-17 10:48:26 -0500

    Add files via upload

--- finetune.py
+++ finetune.py
@@ -6,22 +6,33 @@ Created on Mon Mar  6 19:40:37 2017
 @author: min
 """
 import collections
+import numpy as np
+#First, determine the list of candidates for correct names:
+#dict_temp = dict(collections.Counter(list_names).most_common())
+#list_candidates = sorted(dict_temp, key=.get,reverse=True)[:10]
+#list_candidates = collections.Counter(list_names).keys()
 
-#First, determine the list of candidates for correct names according to your standard for 'famous':
- 
+#You can use the above list as the list of candidates, but for reducing the run-time,
+#I recommend reducing the elements in list_canditates as much as possible.
 list_candidates = [u'ムッシュかまやつ', u'綾小路翔',u'堺正章']
 
 #Next, for every possible pair, count the number of cooccurrence:
-cooccurrence = {}
-for i in list_candidates:
-    for j in list_candidates:
-        if i!=j:
-            cooccurrence[i,j] = 0
-            for n in collections.Counter(list_cooccurrence).keys():
-                if i in n and j in n:
-                    cooccurrence[i,j] += collections.Counter(list_cooccurrence).values()[collections.Counter(list_cooccurrence).keys().index(n)]
 
-print pp(cooccurrence)
-collections
+l=len(list_candidates)
+
+coocc_mat=np.zeros((l, l))
+
+for i in range(l):
+    for j in range(i+1,l):
+        
+        for n in collections.Counter(list_cooccurrence).keys():
+                if list_candidates[i] in n and list_candidates[j] in n:
+                    coocc_mat[i,j] += collections.Counter(list_cooccurrence).values()[collections.Counter(list_cooccurrence).keys().index(n)]
+        coocc_mat[j,i] = coocc_mat[i,j]
+
+for i in range(l):
+    print max(coocc_mat[i,:])/sum(coocc_mat[i,:])
+    
+
 #If person A occurs mostly with person B but not with others while person B occurs with others,
 #then we can infer that B is the deceased person and A comes up only in relation to B, hence eliminate A from the list.
